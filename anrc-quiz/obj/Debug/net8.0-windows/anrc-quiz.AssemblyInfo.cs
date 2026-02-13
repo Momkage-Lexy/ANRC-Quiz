a22e95e515ece4ac63edfa433eccac290fc903f5
Lexy Ramos <lexy.b.quijano@gmail.com>
2026-02-13 13:41:06 -0800

    files

--- anrc-quiz/obj/Debug/net8.0-windows/anrc-quiz.AssemblyInfo.cs
+++ anrc-quiz/obj/Debug/net8.0-windows/anrc-quiz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("anrc-quiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f266fd8bb4d48537d27d40ebf60531cfd5f6e0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e96b6b96b659eec3c46e60c833fcdfd5887265")]
 [assembly: System.Reflection.AssemblyProductAttribute("anrc-quiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("anrc-quiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
