7c2b2a6dbab74e44d8249396cd3df4dbc33ecd78
Lexy Ramos <lexy.b.quijano@gmail.com>
2026-02-18 08:53:31 -0800

    answer formatting

--- anrc-quiz/obj/Debug/net8.0-windows/anrc-quiz.AssemblyInfo.cs
+++ anrc-quiz/obj/Debug/net8.0-windows/anrc-quiz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("anrc-quiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22e96b6b96b659eec3c46e60c833fcdfd5887265")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b981dceca960d4a069cdd3a56fb23723478680b")]
 [assembly: System.Reflection.AssemblyProductAttribute("anrc-quiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("anrc-quiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
